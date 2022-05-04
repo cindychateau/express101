faab66bc39b32b91a6be9d71ca4cee475e13d248
Cynthia Castillo <ca.castillo@gmail.com>
2022-05-04 15:38:32 -0500

    Comentario

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+/* Nos ayudan a interpretar y decodificar el JSON */
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) );
 
